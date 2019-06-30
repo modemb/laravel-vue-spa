8b3dfc379017c0ad9c67883f9d38e041f5dbc028
Mohamed Dembele <modembfr@gmail.com>
2019-06-29 23:22:30 -0400

    added DB SQL

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        for ($i=0; $i < 50000; $i++) { 
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'password' => bcrypt('secret'),
+            ]);
+        }
+
+        // factory(App\User::class, 50)->create()->each(function ($user) {
+        //     $user->posts()->save(factory(App\Post::class)->make());
+        // });
     }
 }
